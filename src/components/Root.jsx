abbc2ffe57527036e8d771a39d561f1cc8cad414
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-05-27 06:27:24 +0600

    Hade sidebar

---
+++ src/components/Root.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { MdDashboard, MdEventAvailable, MdSettings } from "react-icons/md";
+import { BsFillPatchExclamationFill } from "react-icons/bs";
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import { FaHandsHelping } from "react-icons/fa";
+
+const Root = () => {
+  const items = (
+    <>
+      <li className="text-md font-semibold">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+        >
+          <MdDashboard className="text-4xl" />
+          Dashboard
+        </NavLink>
+      </li>
+
+      <li className="text-md font-semibold mt-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+          to="/"
+        >
+          <BsFillPatchExclamationFill className="text-4xl" />
+          Vital Task
+        </NavLink>
+      </li>
+      <li className="text-md font-semibold mt-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+          to="/"
+        >
+          <MdEventAvailable className="text-4xl" />
+          My Task
+        </NavLink>
+      </li>
+      <li className="text-md font-semibold mt-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+          to="/"
+        >
+          <HiBars3BottomLeft className="text-4xl" />
+          Task Categories
+        </NavLink>
+      </li>
+      <li className="text-md font-semibold mt-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+          to="/"
+        >
+          <MdSettings className="text-4xl" />
+          Settings
+        </NavLink>
+      </li>
+      <li className="text-md font-semibold mt-5">
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#ff6867] bg-white w-[95%] px- block rounded-xl p-3 flex items-center gap-2"
+              : "text-white flex items-center gap-2"
+          }
+          to="/"
+        >
+          <FaHandsHelping className="text-4xl" />
+          Help
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="flex p-1 ">
+        <div className="w-full md:w-1/5 border bg-[#ff6767] p-4 h-screen">
+          <ul className="text-white ">{items}</ul>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Root;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>This is Home</h1>,
+    element: <Root />,
   },
 ]);
 
